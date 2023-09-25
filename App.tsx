afd01cca6ad25a039065f62b853ceec7fb891dff
maurotwister <140101450+MauroTwister475@users.noreply.github.com>
2023-09-25 11:16:57 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,53 @@
+import { Notification } from "./components/Notification";
+import { Rocket } from "lucide-react";
+
+export function App() {
+  return (
+    <div className="w-[448px] h-auto rounded bg-zinc-800 m-auto mt-40">
+      <div className="w-full bg-zinc-800 py-4 px-4 flex justify-between">
+        <h1 className="text-white">
+          Notificações
+        </h1>
+        <span className="text-violet-500">
+          Marcar todas como vistas
+        </span>
+      </div>
+      <div className="w-full py-2 bg-zinc-950 px-4">
+        <span className="text-zinc-400 text-sm">
+          Recentes
+        </span>
+      </div>
+      <div className="w-full flex flex-col items-center justify-center">
+        <Notification.Root>
+          <Notification.Icon icon={Rocket} />
+          <Notification.Content text="Você foi convidado para fazer parte de uma empresa chamada SmadCode." />
+          <Notification.Actions />
+        </Notification.Root>
+
+        <Notification.Root>
+          <Notification.Icon icon={Rocket} />
+          <Notification.Content text="Você foi convidado para fazer parte de uma empresa chamada SmadCode." />
+          <Notification.Actions />
+        </Notification.Root>
+      </div>
+      <div className="w-full py-2 bg-zinc-950 px-4">
+        <span className="text-zinc-400 text-sm">
+          Antigas
+        </span>
+      </div>
+      <div className="w-full flex flex-col items-center justify-center">
+        <Notification.Root>
+          <Notification.Icon icon={Rocket} />
+          <Notification.Content text="Você foi convidado para fazer parte de uma empresa chamada SmadCode" />
+          <Notification.Actions />
+        </Notification.Root>
+
+        <Notification.Root>
+          <Notification.Content text="Você foi convidado para fazer parte de uma empresa chamada SmadCode" />
+          <Notification.Actions />
+        </Notification.Root>
+      </div>
+    </div>
+  )
+}
+
